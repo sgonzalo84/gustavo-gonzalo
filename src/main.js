f7c89f238b6cfb1319e7afb29357b42ba209bf09
sgonzalo84 <sgonzalo84@gmail.com>
2026-06-10 18:12:49 +0200

    Añadida sección del libro. Mejoras en estilos e imagenes.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-import "./style.css";
-import javascriptLogo from "./assets/javascript.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import { setupCounter } from "./counter.js";
-
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
